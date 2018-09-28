0b7e5afac0e973bc24c8a168f9e3840597be364b
zfgaaa <1248639502@qq.com>
2018-09-28 21:11:36 +0800

    asdsad

--- js/register.js
+++ js/register.js
@@ -1,11 +1,14 @@
-var login = (function(){
+var register = (function(){
     return {
         init(ele){
             this.$ele = document.querySelector(ele);
+            this.$usernameTable = document.querySelector('.username');
+            console.log(this.$usernameTable);
             this.event();
         },
         event(){
             var _this = this;
         }
+
     }
 }())
